e10283c44ce60d2139659c44cf568d784a69af4c
Gary McAdam <gary.mcadam@sky.uk>
2016-06-03 12:54:25 +0100

    return formatted slack messages

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const partridge = require('./partridge');
 const restify = require('restify');
 const server = restify.createServer();
 
-server.get('/:str', (req, res, next) => {
-    partridge('protest')
+const response = (req, res, next) => {
+    partridge(req.params.text)
         .then(result => {
-            res.send(result);
+            res.send({
+                text: result.gif
+            });
         });
-});
+}
+
+server.post('/', response);
+server.get('/', response);
 
 server.listen(process.env.PORT || 8080);
